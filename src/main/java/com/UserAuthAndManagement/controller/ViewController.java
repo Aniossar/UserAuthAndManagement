3f0093e82e4969ccaba3dcc0540baecc6f1fc72c
Aniossar <66470276+Aniossar@users.noreply.github.com>
2023-01-30 19:16:18 +0400

    Update ViewController.java

--- src/main/java/com/UserAuthAndManagement/controller/ViewController.java
+++ src/main/java/com/UserAuthAndManagement/controller/ViewController.java
@@ -1,8 +1,15 @@
 package com.UserAuthAndManagement.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class ViewController {
 
+  @GetMapping("/changePassword?token={restoreToken}")
+    public String restorePasswordPage(@PathVariable String restoreToken) {
+        return "restorePass";
+    }
+  
 }
